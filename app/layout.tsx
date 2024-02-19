e52077b613d147f6760e6e5e48b6d838e36a8a0f
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-02-19 15:25:44 +0530

    added auth with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
-
-
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
-const IBMPlex = IBM_Plex_Sans({ 
+const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-ibm-plex'
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
 });
 
 export const metadata: Metadata = {
@@ -23,11 +22,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
- 
+    <ClerkProvider>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
         </body>
       </html>
+    </ClerkProvider>
   );
 }
