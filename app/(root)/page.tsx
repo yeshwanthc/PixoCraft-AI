9ea918b18b0b0d26c5b0763d3b545cefebd44af6
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-02-20 22:01:34 +0530

    commit #8

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      Home
-    </main>
+    <div>
+      <p>Home</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
