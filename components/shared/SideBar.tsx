8c531ac6cf5e260c3629ef0377c1cf127d82a72e
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-02-23 22:15:08 +0530

    commit #11

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { navLinks } from "../../constants";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 
 const SideBar = () => {
+  const pathname = usePathname();
   return (
     <aside className="sidebar">
       <div className="flex size-full flex-col gap-4">
@@ -10,9 +16,45 @@ const SideBar = () => {
             src="/assets/images/Pixo_Craft__3.png"
             alt="Logo"
             height={65}
-            width={280}
+            width={180}
           />
         </Link>
+        <nav className="sidebar-nav">
+          <SignedIn>
+            <ul className="sidebar-nav_elements">
+              {navLinks.map((link) => {
+                const isActive = link.route === pathname;
+
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-600"
+                    }`}
+                  >
+                    <Link href={link.route} className="sidebar-link">
+                      <Image
+                        src={link.icon}
+                        alt="Icon"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </SignedIn>
+          <SignedOut>
+            <Button asChild>
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </nav>
       </div>
     </aside>
   );
