23495ebda5616124cdb64b9456a508abea632364
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-02-26 22:28:08 +0530

    commit #14

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
-  )
-}
+    <header className="header">
+      <Link href="/" className="flex items-center gap-2">
+        <Image
+          src="/assets/images/Pixo_Craft__3.png"
+          alt="Logo"
+          width={180}
+          height={28}
+        />
+      </Link>
+      <nav className="flex gap-2">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+      </nav>
+    </header>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
