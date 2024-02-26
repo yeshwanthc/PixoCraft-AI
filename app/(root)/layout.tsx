3b68eb9890129bcb37bc716aa41bb777ec134457
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-02-26 22:14:16 +0530

    commit #13

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,11 @@
+import MobileNav from "../../components/shared/MobileNav";
 import SideBar from "../../components/shared/SideBar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <SideBar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
-import logo from "../../public/assets/Pixo_Craft_Logo.png"
+import logo from "../../public/assets/Pixo_Craft_Logo.png";
 
 export default function Home() {
   return (
     <div>
-     
       <p>Home</p>
     </div>
   );
