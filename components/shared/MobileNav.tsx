61f9f92b23d1b34b958bce49229ad5443b7cbf2d
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-02-26 22:46:56 +0530

    commit #15 mobile menu with scadcn

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   Sheet,
@@ -9,9 +11,13 @@ import {
 } from "../ui/sheet";
 import Link from "next/link";
 import Image from "next/image";
-import { SignedIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { navLinks } from "../../constants";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 
 const MobileNav = () => {
+  const pathname = usePathname();
   return (
     <header className="header">
       <Link href="/" className="flex items-center gap-2">
@@ -25,7 +31,55 @@ const MobileNav = () => {
       <nav className="flex gap-2">
         <SignedIn>
           <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent className="sheet-content sm:w-64">
+              <Image
+                src="/assets/images/Pixo_Craft__3.png"
+                alt="Logo"
+                width={160}
+                height={24}
+              />
+              <ul className="header-nav_elements">
+                {navLinks.map((link) => {
+                  const isActive = link.route === pathname;
+
+                  return (
+                    <li
+                      key={link.route}
+                      className={`${
+                        isActive && "gradient-text"
+                      } p-18 flex whitespace-nowrap text-dark-700`}
+                    >
+                      <Link href={link.route} className="sidebar-link cursor-pointer">
+                        <Image
+                          src={link.icon}
+                          alt="Icon"
+                          width={24}
+                          height={24}
+                        />
+                        {link.label}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </SheetContent>
+          </Sheet>
         </SignedIn>
+        <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
       </nav>
     </header>
   );
