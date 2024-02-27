5d1d2975f226b28f46530d48422a2c4aeb8e1e4e
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-02-28 00:49:56 +0530

    empty

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -15,3 +15,20 @@ if (!cached) {
     promise: null,
   };
 }
+
+export const connectToDataBase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "pixocraft",
+      bufferCommands: false,
+    });
+
+    cached.conn= await cached.promise;
+
+    return cached.conn;
+};
